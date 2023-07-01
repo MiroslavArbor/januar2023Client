e5de58258b5b3cdae0c9da05e0ccc1b56915b621
MiroslavArbor <miroslav.jovanovic@arbor-education.com>
2023-07-01 12:28:12 +0200

    Client side of a project for java exam in January

---
+++ src/model/ModelTabeleAngazovanjaIzmena.java
@@ -0,0 +1,108 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import domen.Angazovanje;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import javax.swing.table.AbstractTableModel;
+
+/**
+ *
+ * @author Mafija
+ */
+public class ModelTabeleAngazovanjaIzmena extends AbstractTableModel{
+
+    ArrayList<Angazovanje> lista;
+    SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
+    
+    public ModelTabeleAngazovanjaIzmena(){
+        lista = new ArrayList<>();
+    }
+
+    public ModelTabeleAngazovanjaIzmena(ArrayList<Angazovanje> lista) {
+        this.lista = lista;
+        fireTableDataChanged();
+    }
+    
+    
+    
+    @Override
+    public int getRowCount() {
+        return lista.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return 4;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        Angazovanje a = lista.get(rowIndex);
+        
+        switch (columnIndex) {
+            case 0: return a.getPredmet().getNazivPredmeta();
+            case 1: 
+                if(a.getProfesor() != null){
+                    return a.getProfesor();
+                }else{
+                    return "Nije jos unet profesor";
+                }
+            case 2: return sdf.format(a.getDatum());
+            case 3: return a.getStatus();
+            default:
+                return "N/A";
+        }
+    }
+
+    @Override
+    public String getColumnName(int column) {
+        switch (column) {
+            case 0:
+                return "Predmet";
+            case 1: return "Profesor";
+            case 2: return "Datum";
+            case 3: return "Status";
+            default:
+                return "N/A";
+        }
+    }
+
+    public ArrayList<Angazovanje> getLista() {
+        return lista;
+    }
+    
+    public void dodajAngazovanje(Angazovanje a){
+        a.setStatus("NOVI");
+        lista.add(a);
+        fireTableDataChanged();
+    }
+    
+    public void obrisiAngazovanje(int red){
+        Angazovanje a = lista.get(red);
+        if (a.getStatus().equals("NOVI")) {
+           lista.remove(red);
+        }else{
+            lista.get(red).setStatus("BRISANJE");
+        }
+        fireTableDataChanged();
+    }
+
+    public Angazovanje vratiAngazovanje(int red) {
+        return lista.get(red);
+    }
+
+    public void izmeniAngazovanje(Angazovanje a) {
+        for (Angazovanje angazovanje : lista) {
+            if (angazovanje.getId() == a.getId()) {
+                angazovanje.setDatum(a.getDatum());
+                angazovanje.setStatus("IZMENA");
+            }
+        }
+        fireTableDataChanged();
+    }
+    
+}
